a69190decb8bcc64e8cc8a992655ce0f4e807df7
Isaac Dream <isaackprogramming44524d@gmail.com>
2025-12-27 23:41:35 +0200

    finished the console logic, added some functions

--- script.js
+++ script.js
@@ -11,6 +11,52 @@ let getHumanChoice = function () {
 
 let getComputerChoice = function () {
     const randomIndex = Math.floor(Math.random() * choices.length);
-    const computerChoice = choices[randomIndex];
+    const computer = choices[randomIndex];
+    return computer;
 };
 
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function getWinner() {
+        if (humanScore > computerScore) {console.log("You Win The Game, Congratulations.");}
+
+        else if (computerScore > humanScore) {console.log("You lose, Computer Won. Better Luck Next time.");}
+
+        else {console.log("Its a Tie");}
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            humanScore = 0, computerScore = 0;
+            return "Tie";
+        }
+
+        else if (
+            (humanChoice === "rock" && computerChoice === "scissor") ||
+            (humanChoice === "scissor" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "rock")
+        ) {
+            humanScore += 1, computerChoice += 0;
+            return "player";
+        }
+
+        else {
+            computerScore += 1, humanScore += 0;
+            return "computer";
+        }
+    }
+
+    for (let i = 1; i <= 5; i++) {
+        const playerSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection));
+        
+    } const results = getWinner(); console.log(results)
+
+};
+
+playGame()
